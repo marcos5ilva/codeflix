f38e0e97ca1d8efa684791a5aef52e2f576739a0
Marcos Silva <marcos.silva79@gmail.com>
2020-07-27 23:02:15 -0400

    first commit

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ButtonLink = ({
+        className,
+        href,
+        children
+    }) => {
+        return ( < a className = {
+                className
+            }
+            href = {
+                href
+            } > {
+                children
+            } < /a>)
+        }
+
+        export default ButtonLink;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Logo from '../../assets/Logo.png';
+/*import ButtonLink from './components/ButtonLink'*/
+import Button from '../Button';
+import './Menu.css';
+
+
+
+const Menu = () => {
+    return ( <
+        nav className = "Menu" >
+        <
+        a href = "/" >
+        <
+        img className = "Logo"
+        src = {
+            Logo
+        }
+        alt = "Codeflix logo" / >
+        <
+        /a> <
+        Button as = "a"
+        href = "/"
+        className = "ButtonLink" > New video < /Button> < /
+        nav >
+    )
+}
+
+export default Menu;
