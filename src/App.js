eb556e311171505db383f2d937d25fdfa5cfe9c7
Marcos Silva <marcos.silva79@gmail.com>
2020-07-28 14:49:58 -0400

    Added MainMenu, Carousel, and Footer components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import Menu from './components/Menu'
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+import Data from './data/dados_iniciais.json';
 
 
 function App() {
-  return ( < div > < Menu / > < /div>);
+  return ( < div > < Menu / >
+    <
+    BannerMain videoTitle = {
+      Data.categorias[0].videos[0].titulo
+    }
+    url = {
+      Data.categorias[0].videos[0].url
+    }
+    videoDescription = {
+      "What 's front-end?"
+    }
+    / > <
+    Footer / > <
+    / div>);
   }
 
   export default App;
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Logo from '../../assets/Logo.png';
-/*import ButtonLink from './components/ButtonLink'*/
 import Button from '../Button';
 import './Menu.css';
 
